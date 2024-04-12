dd041c642b56fc8193c501b96f58bfa0010eab81
Zhangzw999 <zhangzw0170@gmail.com>
2024-04-12 22:23:03 +0800

    update

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,15 +11,15 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.entry(String::from("apple")).or_insert(2);
+    basket.entry(String::from("mango")).or_insert(2);
 
     // TODO: Put more fruits in your basket here.
 
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    output.push( string.to_uppercase() );
+                },
+                Command::Trim => {
+                    output.push( string.trim().to_string() );
+                },
+                Command::Append(n) => {                         // n became &usize in the match control flow
+                    let mut str_mut = String::from(string);     // s.push() requires &mut 
+                    for _ in 0..(*n) {                          // here n requires dereference
+                        str_mut = str_mut + "bar";              // the semantics of str_mut will be moved to the left side
+                    }
+                    output.push( str_mut );
+                },
+            }
         }
         output
     }
@@ -45,7 +58,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
